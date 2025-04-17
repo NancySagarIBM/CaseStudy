f5ad20c623d74d83e770aca601a28c96973980fa
Nancy Sagar <Nancy.Sagar@ibm.com>
2025-04-17 11:13:12 +0000

    Committ

--- models/casestudy/casestudy.sql
+++ models/casestudy/casestudy.sql
@@ -1,7 +1,6 @@
 SELECT
     Product_ID AS Product_ID,
     Product_Name AS Product_Name,
-    t2.Product_Name AS Product_Name_t2,
     Category AS Category,
     Price AS Price,
     Stock AS Stock
@@ -10,8 +9,7 @@ UNION
 SELECT
     Product_ID AS Product_ID,
     Product_Name AS Product_Name,
-    t2.Product_Name AS Product_Name_t2,
     Category AS Category,
     Price AS Price,
-    Stock AS Stock
+    Stock AS Stock FROM
 MYDB.CASE_POSTGRES_PUBLIC.STG_PRODUCTS
